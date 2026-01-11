0703729528f068dc07147bf6584623a411a90122
Christian Falch <christian.falch@gmail.com>
2026-01-11 17:05:38 +0100

    Added mono input detection

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -20,13 +20,43 @@ extern void BindProcessorBuffers(AudioProcessor &processor);
 
 PatchWireDesc MakeExamplePatch();
 PatchWireDesc MakePassthroughPatch();
+
+// Mono input detection: copies left channel to right when right is silent
+// This handles the common case of a mono guitar input on a stereo codec
 static void AudioCallback(daisy::AudioHandle::InputBuffer in,
                           daisy::AudioHandle::OutputBuffer out,
                           size_t size)
 {
     g_midi.ApplyPendingInAudioThread();
 
-    g_processor.ProcessBlock(in, out, size);
+    // Detect mono input: check if right channel is essentially silent while left has signal
+    float leftEnergy = 0.0f, rightEnergy = 0.0f;
+    for (size_t i = 0; i < size; ++i)
+    {
+        leftEnergy += in[0][i] * in[0][i];
+        rightEnergy += in[1][i] * in[1][i];
+    }
+
+    // If left has significant signal but right is near-silent, treat as mono
+    bool monoInput = (leftEnergy > 1e-8f && rightEnergy < leftEnergy * 0.001f);
+
+    if (monoInput)
+    {
+        // Process with left channel duplicated to right
+        for (size_t i = 0; i < size; ++i)
+        {
+            float inL = in[0][i];
+            float outL, outR;
+            g_processor.ProcessFrame(inL, inL, outL, outR);
+            out[0][i] = outL;
+            out[1][i] = outR;
+        }
+    }
+    else
+    {
+        // Normal stereo processing
+        g_processor.ProcessBlock(in, out, size);
+    }
 }
 
 int main()
